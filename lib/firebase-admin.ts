dad40c56da9474390b6b1f4c516c9120fb1dc8e9
Arjun Prasad <arjunmishra769@gmail.com>
2026-06-07 01:25:44 +0530

    change

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -5,17 +5,11 @@ import { getAuth } from 'firebase-admin/auth'
 function initAdminApp() {
   if (getApps().length > 0) return getApp()
 
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY
-
-  if (!privateKey) {
-    throw new Error('FIREBASE_ADMIN_PRIVATE_KEY is not set')
-  }
-
   return initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: privateKey.replace(/\\n/g, '\n'), // ← \n properly replace
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
     }),
   })
 }
